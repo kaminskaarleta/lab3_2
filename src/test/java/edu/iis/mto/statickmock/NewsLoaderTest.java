254d84fec38f9e32668c9fd61d86b4d72f13dcf8
Kamińska <180195@edu.p.lodz.pl>
2015-04-12 13:39:05 +0200

    Added behaviour test in NewsLoaderTest class for validate number of run getReader static method.

--- src/test/java/edu/iis/mto/statickmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/statickmock/NewsLoaderTest.java
@@ -1,12 +1,14 @@
 package edu.iis.mto.statickmock;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -27,45 +29,62 @@ import static org.mockito.Mockito.verify;
 @PrepareForTest( { ConfigurationLoader.class, NewsReaderFactory.class } )
 
 public class NewsLoaderTest {
-
+	
+	NewsLoader newsLoader;
+	IncomingNews incomingNews;
+	
+	ConfigurationLoader configurationLoader;
+	NewsReaderFactory newsReaderFactory;
+	Configuration config;
+	NewsReader reader;
+	
+	
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void stateTest_validateSegregationTypesOfNews() {
 		mockStatic(ConfigurationLoader.class);
 		mockStatic(NewsReaderFactory.class);
 		
-		NewsLoader newsLoader = new NewsLoader();
-		IncomingNews incomingNews = new IncomingNews();
+		newsLoader = new NewsLoader();
+		incomingNews = new IncomingNews();
+		
 		incomingNews.add(mockIncomingInfo(SubsciptionType.A));
 		incomingNews.add(mockIncomingInfo(SubsciptionType.A));
 		incomingNews.add(mockIncomingInfo(SubsciptionType.B));
 		incomingNews.add(mockIncomingInfo(SubsciptionType.NONE));
 
+		configurationLoader = mock(ConfigurationLoader.class);
+		newsReaderFactory = mock(NewsReaderFactory.class);
+		config = mock(Configuration.class);
+		reader = mock(NewsReader.class);
 		
-		ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
-		NewsReaderFactory newsReaderFactory = mock(NewsReaderFactory.class);
-		Configuration config = mock(Configuration.class);
-		NewsReader reader = mock(NewsReader.class);
-
 		
 		when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
-		when(ConfigurationLoader.getInstance().loadConfiguration()).thenReturn(config);
+		when(configurationLoader.loadConfiguration()).thenReturn(config);
 		
 		when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(reader);
 		when(reader.read()).thenReturn(incomingNews);
-		
-		PublishableNews publishableNews = newsLoader.loadNews();
 	}
 
-	private IncomingInfo mockIncomingInfo(SubsciptionType type){
-		
+	@Test
+	public void stateTest_validateSegregationTypesOfNews() {	
+		PublishableNews publishableNews = newsLoader.loadNews();
+		assertThat(publishableNews.getPublicContent().size(), is(1));
+		assertThat(publishableNews.getSubscribentContent().size(), is(3));
+	}
+	
+	@Test
+	public void behaviorTest_getReaderRunNumer_shouldBeOne() throws Exception {		
+		PublishableNews publishableNews = newsLoader.loadNews();
+		PowerMockito.verifyStatic(Mockito.times(1));	
+		NewsReaderFactory.getReader(Mockito.anyString());	
+	}
+
+	private IncomingInfo mockIncomingInfo(SubsciptionType type){	
 		IncomingInfo incomingInfo = mock(IncomingInfo.class);
 		when(incomingInfo.requiresSubsciption()).thenReturn(type != SubsciptionType.NONE);
 		when(incomingInfo.getSubscriptionType()).thenReturn(type);
