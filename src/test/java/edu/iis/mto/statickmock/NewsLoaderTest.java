3d3a86be9d444e21043bf469c534cc807a4e6257
Kamińska <180195@edu.p.lodz.pl>
2015-04-12 11:46:03 +0200

    Prepared data to state test which is used for validate segregation types of news.

--- src/test/java/edu/iis/mto/statickmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/statickmock/NewsLoaderTest.java
@@ -5,11 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
+import edu.iis.mto.staticmock.IncomingNews;
+import edu.iis.mto.staticmock.NewsLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
+import edu.iis.mto.staticmock.SubsciptionType;
+import edu.iis.mto.staticmock.reader.NewsReader;
 import static org.powermock.api.mockito.PowerMockito.*;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest( SingletonService.class )
 public class NewsLoaderTest {
 
 	@BeforeClass
@@ -22,7 +35,37 @@ public class NewsLoaderTest {
 
 	@Test
 	public void test() {
+		mockStatic(ConfigurationLoader.class);
+		mockStatic(NewsReaderFactory.class);
+		
+		NewsLoader newsLoader = new NewsLoader();
+		IncomingNews incomingNews = new IncomingNews();
+		incomingNews.add(mockIncomingInfo(SubsciptionType.A));
+		incomingNews.add(mockIncomingInfo(SubsciptionType.A));
+		incomingNews.add(mockIncomingInfo(SubsciptionType.B));
+		incomingNews.add(mockIncomingInfo(SubsciptionType.C));
 
+		
+		ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+		NewsReaderFactory newsReaderFactory = mock(NewsReaderFactory.class);
+		Configuration config = mock(Configuration.class);
+		NewsReader reader = mock(NewsReader.class);
+
+		
+		when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+		when(ConfigurationLoader.getInstance().loadConfiguration()).thenReturn(config);
+		
+		when(NewsReaderFactory.getReader(config.getReaderType())).thenReturn(reader);
+		when(reader.read()).thenReturn(incomingNews);
+		
+		PublishableNews publishableNews = newsLoader.loadNews();
 	}
 
+	private IncomingInfo mockIncomingInfo(SubsciptionType type){
+		
+		IncomingInfo incomingInfo = mock(IncomingInfo.class);
+		when(incomingInfo.getSubscriptionType()).thenReturn(type);
+		
+		return incomingInfo;
+	}
 }
